3d2ab3ed2595166f2c283d6538fb08525aecf075
Pedro Henrique <phlferezin03@gmail.com>
2024-05-25 20:38:53 -0300

    fix: retirando campo email

--- src/views/fale-conosco/script.js
+++ src/views/fale-conosco/script.js
@@ -2,12 +2,11 @@ document.getElementById("formulario").addEventListener("submit", function () {
   event.preventDefault();
 
   const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
   const mensagem = document.getElementById("mensagem-texto").value;
 
   const link = `mailto:associacaocaixadagua1997@gmail.com?subject=Mensagem de ${encodeURIComponent(
     nome
-  )}. Email: ${encodeURIComponent(email)}&body=${encodeURIComponent(mensagem)}`;
+  )}.&body=${encodeURIComponent(mensagem)}`;
 
   window.location.href = link;
 });
